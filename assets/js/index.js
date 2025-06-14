1cb82c0395351228ba5a716f104c5817fbc67fbd
IrynaKr1 <krutsiryna@gmail.com>
2025-06-14 14:30:30 +0200

    Delete elements form the beginning and end of array, adding new elements on beginning and end of array, task 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,12 @@ for (let i = 0; i < NUMBERS_COUNT; i++) {
 }
 
 console.log(numbers);
+
+// Видалити останній і початковий елемент з масиву, додати елемент до початку і кінця.
+
+console.log('Видалити останній елемент з масиву: ', numbers.pop());
+console.log('Видалити початковий елемент з масиву: ', numbers.shift());
+numbers.unshift(452);
+console.log('Додати елемент до початку', numbers);
+numbers.push(678);
+console.log('Додати елемент до кінця', numbers);
